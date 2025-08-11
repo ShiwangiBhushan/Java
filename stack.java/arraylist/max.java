1dcdaf6b5002aa27deb360fd9772d88393a0699a
ShiwangiBhushan <135050613+ShiwangiBhushan@users.noreply.github.com>
2025-08-11 01:47:14 +0000

    c

---
+++ stack.java/arraylist/max.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class arraylist {
+    /* array => fix size and stores primitive datatypes
+    arraylist => dynamic size & no storage of primitives
+     */
+    public static void main(String[] args) {
+        // Classname objectName = new ClassName();
+        ArrayList<Integer> list6 = new ArrayList<>();
+        /* operations
+        add , get(to get the index of) , remove ,
+        set element at index , contains element
+         */
+
+        ArrayList <Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        list.add(9);
+
+        // to return the max element
+
+        int MAX = Integer.MIN_VALUE;
+        for (int i =0; i<list.size(); i++) {
+            if(MAX < list.get(i)) {
+                MAX = list.get(i);
+            }
+        }
+        //System.out.println("max element = " + MAX);
+
+        
+    }
+}
