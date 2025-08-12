733bb8ab20e565a260345d90b3496cf5c6a2cbdd
ShiwangiBhushan <135050613+ShiwangiBhushan@users.noreply.github.com>
2025-08-12 03:47:19 +0000

    c

---
+++ arraylist/swap.java
@@ -0,0 +1,23 @@
+public class swap {
+    public static void main (String [] args) {
+        int idx = 1 , idx2 = 2;
+        System.out.print(list);
+        swap(list ,idx1,idx2);
+        System.out.print(list);
+
+        //ascending order
+        Collections.sort(list);
+        //descending order
+        Collections.sort(list, Collections.reverseOrder());
+
+        //swap 2 numbers
+    public static void swap (ArrayList<Integer> list, int idx1, int idx2) {
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2,temp);
+    }
+
+    
+    }
+    
+}
