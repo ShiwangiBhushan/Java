5e0035374ea4d4ce327bcedc286792e4f54ef0a0
ShiwangiBhushan <135050613+ShiwangiBhushan@users.noreply.github.com>
2025-08-14 08:28:13 +0000

    c

--- bitmanipulation/bits.java
+++ bitmanipulation/bits.java
@@ -1,5 +1,16 @@
 public class bits {
     public static void main(String[] args) {
-        
+        System.out.print(countSetBits(7));
     }
+    // to count for the set bits
+    public static int countSetBits(int n) {
+        int count  = 0;
+        while (n>0) {
+            if((n&1) != 0) {
+                count++;
+            }
+            n = n>>1;
+        }
+        return count;
+
 }
