36c737a051c0585b08ff598bbd09b8d4aba28276
ShiwangiBhushan <135050613+ShiwangiBhushan@users.noreply.github.com>
2025-08-18 02:34:58 +0000

    c

--- bitmanipulation/formulas.java
+++ bitmanipulation/formulas.java
@@ -3,6 +3,12 @@ public class formulas {
         /* to remember
         1&1 = 1
         0|0 = 0
-        0^1 = 1 */
+        0^1 = 1 
+
+        for even/odd
+        bitmask = 1
+        n& bitmask == 0 ->even
+        
+        */
     }
 }
