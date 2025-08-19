3a952e0af510ab7a33bb0c567399fd94c8b1e317
ShiwangiBhushan <135050613+ShiwangiBhushan@users.noreply.github.com>
2025-08-19 13:05:29 +0000

    c

--- arraylist/swap.java
+++ arraylist/swap.java
@@ -14,6 +14,7 @@ public class swap {
     public static void swap (ArrayList<Integer> list, int idx1, int idx2) {
         int temp = list.get(idx1);
         list.set(idx1, list.get(idx2));
+        
         list.set(idx2,temp);
     }
 
